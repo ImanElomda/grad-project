6b4d3cc63442e2d1be8a4485405aa4c72924892b
ImanElomda <emanelomda9@gmail.com>
2023-05-16 17:06:06 +0300

    commit10

--- modules/student/student.route.js
+++ modules/student/student.route.js
@@ -3,5 +3,5 @@ import * as uc from './controller/student.controller.js'
 const router = Router()
 
 router.post("/addUser",uc.addStudent)
-router.get("/getQuestion",uc.studentQuestion)
+router.get("/getQuestion/:KolbStyle/:GPDK",uc.studentQuestion)
 export default router
