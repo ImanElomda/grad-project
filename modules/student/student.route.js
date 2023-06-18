a34ecb68b79bd3645599d05eb3784c302082a1bb
ImanElomda <emanelomda9@gmail.com>
2023-06-18 23:57:00 +0300

    commit fro2

--- modules/student/student.route.js
+++ modules/student/student.route.js
@@ -3,4 +3,4 @@ import * as uc from './controller/student.controller.js'
 const router = Router()
 
 router.post("/addUser",uc.addStudent)
-router.get("/getQuestion/:KolbStyle/:GPDK",uc.studentQuestion)
+router.post("/getQuestion/:KolbStyle/:GPDK",uc.studentQuestion)
