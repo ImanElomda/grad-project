db5047b973aaa053e115c74c2caf474b7175660a
ImanElomda <emanelomda9@gmail.com>
2023-06-23 01:32:21 +0300

    router commit

--- modules/student/student.route.js
+++ modules/student/student.route.js
@@ -4,8 +4,4 @@ const router = Router()
 
 router.post("/addUser",uc.addStudent)
 router.post("/getQuestion/:KolbStyle/:GPDK",uc.studentQuestion)
-<<<<<<< HEAD
 export default router
-=======
-export default router
->>>>>>> 4fe530ac4f66e687cfa8ed7a9b08bc6a9b7d89ac
